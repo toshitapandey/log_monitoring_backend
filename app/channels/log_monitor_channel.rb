38c4251919dccc0a53b200f644d474f4d0885db3
Toshita Pandey <toshitapandey@Apples-MacBook-Air.local>
2022-07-17 20:59:30 +0530

    use tail -f to fetch the last lines

--- app/channels/log_monitor_channel.rb
+++ app/channels/log_monitor_channel.rb
@@ -1,17 +1,15 @@
 class LogMonitorChannel < ApplicationCable::Channel
   def subscribed
     stream_from "log_monitor" 
+
     log_file_path = Rails.root.join('log/development.log').to_s
 
-    file_content = File.open(log_file_path).readlines
-    ActionCable.server.broadcast('log_monitor', { log: file_content.last(20), type: "current_log" } )
+    ActionCable.server.broadcast('log_monitor', { log: added_lines(log_file_path), type: "new_log" } )
 
     Filewatcher.new([log_file_path]).watch do |_file_path, event_type|
       next unless event_type.to_s.eql?('updated')
 
-      file_lines = added_lines(log_file_path)
-
-      ActionCable.server.broadcast('log_monitor', { log: file_lines, type: "new_log" } )
+      ActionCable.server.broadcast('log_monitor', { log: added_lines(log_file_path), type: "new_log" } )
     end
   end
 
@@ -20,8 +18,6 @@ class LogMonitorChannel < ApplicationCable::Channel
   end
 
   def added_lines(file_path)
-    file_content = File.open(file_path).readlines
-
-    file_content.last(20)
+    `tail -n 20 #{file_path}`
   end
 end
